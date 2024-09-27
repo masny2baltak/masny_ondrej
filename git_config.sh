7af81be9368c3834d77e030a9f88318bb82bdf25
andrew greasy <masny@skolabaltaci.cz>
2024-09-27 08:20:53 +0200

    hello world

---
+++ git_config.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo "Welcome to Git config"
+git config --global user.email "masny@skolabaltaci.cz"
+git config --global user.name "andrew greasy"
+git config --list
+echo "Done successfully"
